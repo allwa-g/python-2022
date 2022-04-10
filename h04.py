f293acd2c6202465c23c5034277a32c8e87287fe
juliasicak <100847480+juliasicak@users.noreply.github.com>
2022-04-10 11:48:26 +0200

    Update h04.py

--- h04.py
+++ h04.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Meno: Sičáková, Júlia
-# Spolupráca: 
-# Použité zdroje: prednasky, stackoverflow
-# Čas: 2 hod
-
 # Podrobný popis je dostupný na: https://github.com/ianmagyar/introduction-to-python/blob/master/assignments/homeworks/homework4.md
 
 
