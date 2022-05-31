e7318ddfccc3336bd660b24081a5733277a89ba4
allwa-g <100847480+allwa-g@users.noreply.github.com>
2022-05-31 15:24:08 +0200

    Update h09.py

--- h09.py
+++ h09.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Meno: Sičáková, Júlia
-# Spolupráca:
-# Použité zdroje:
-# Čas:
-
 # Podrobný popis je dostupný na: https://github.com/ianmagyar/introduction-to-python/blob/master/assignments/homeworks/homework9.md
 
 
